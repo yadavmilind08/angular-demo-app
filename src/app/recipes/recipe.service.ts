c406649e686403a0ded6f01574a316ba8e43fd8b
Milind Yadav <yadavmilind08@gmail.com>
2021-03-26 18:27:21 +0530

    Using Shopping list service for cross component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter();
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
@@ -10,7 +11,6 @@ export class RecipeService {
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
     )
   ];
-  recipeSelected = new EventEmitter();
 
   getRecipes() {
     return this.recipes.slice();
