89c332454e013539fb2a45297d71bb1479afbf94
Milind Yadav <yadavmilind08@gmail.com>
2021-04-07 12:44:42 +0530

    Clear the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -42,6 +42,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     form.reset();
   }
 
+  onClear() {
+    this.editMode = false;
+    this.slForm?.reset();
+  }
+
   ngOnDestroy() {
     this.subscription?.unsubscribe();
   }
