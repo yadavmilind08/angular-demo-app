5187ae3cd4976ef0d1b1e3e179546ed978dda206
Milind Yadav <yadavmilind08@gmail.com>
2021-04-15 19:45:36 +0530

    Submitting Recipe Edit form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,6 +15,9 @@ export class RecipeListComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    })
     this.recipes = this.recipeService.getRecipes();
   }
 
