78a769557dad9fa6be53cf1f8a8f91f8d3d4db13
Milind Yadav <yadavmilind08@gmail.com>
2021-05-05 18:07:34 +0530

    Prepared signup request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB2DNhWvKkAYXQt_DoCwjLBoBHNek1WewI',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
+}
