82cf4935fb86adf24ea7a37a2c6b92ff1d41b198
Milind Yadav <yadavmilind08@gmail.com>
2021-06-09 18:14:38 +0530

    Attaching token with an interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -22,20 +22,16 @@ export class DataStorageService {
   fetchRecipes() {
     // take gives data only once and after that it will automatically unsubscribe
     // exhaustMap waits for first observable to finish, after that it takes data from it to use in second observable
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>('https://recipe-book-api-6107d-default-rtdb.firebaseio.com/recipes.json', {
-          params: new HttpParams().set('auth', user.token)
-        })
-      }),
-      map(recipes => {
-        return recipes.map(recipe => {
-          return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      }))
+
+    return this.http.get<Recipe[]>('https://recipe-book-api-6107d-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        }))
   }
 }
