98017ddbceb4bed41aa831a6db3eba13fad5ec59
Milind Yadav <yadavmilind08@gmail.com>
2021-06-10 19:22:49 +0530

    Removed exports as we dont use any component from feature module in app module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -19,14 +19,6 @@ import { RecipesComponent } from "./recipes.component";
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
-  ]
+  imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule]
 })
 export class RecipesModule { }
