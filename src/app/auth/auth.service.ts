057aa6eb8258a47545606f9debb96ec4ad243165
Milind Yadav <yadavmilind08@gmail.com>
2021-06-11 18:57:03 +0530

    added environment variables

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
+import { environment } from "../../environments/environment";
 
 import { User } from './user.model';
 
@@ -26,7 +27,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB2DNhWvKkAYXQt_DoCwjLBoBHNek1WewI',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPTKey,
         {
           email: email,
           password: password,
@@ -49,7 +50,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB2DNhWvKkAYXQt_DoCwjLBoBHNek1WewI', {
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPTKey, {
         email: email,
         password: password,
         returnSecureToken: true
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPTKey: 'AIzaSyB2DNhWvKkAYXQt_DoCwjLBoBHNek1WewI'
 };
